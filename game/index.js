ee2e47f156de02aa44cd1b335e7ef2704708eada
damon-x <dxbluestar@yandex.com>
2021-06-25 13:53:39 +0800

    fix

--- game/index.js
+++ game/index.js
@@ -32,7 +32,7 @@ var defaults = {
   materials: ['#fff', '#000'],
   materialFlatColor: true,
   worldOrigin: [0, 0, 0],
-  playerSkin:'man.png',
+  playerSkin:'man.json',
   controls: { discreteFire: true }
 }
 var opts = extend({}, defaults, opts || {})
